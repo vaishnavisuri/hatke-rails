7b85baad6835d3be74e1ebb2b98ff300baa14c14
Pothem.Mallesham <pothem.mallesham@gmail.com>
2018-06-05 00:43:28 +0530

    refactoring

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,22 @@
 // about supported directives.
 //
 //= require jquery
-//= require rails-ujs
-//= require theme
-//= require_tree .
+//= require theme/bootstrap.min
+//= require theme/jquery.isotope.min
+//= require theme/jquery.prettyPhoto
+//= require theme/easing
+//= require theme/jquery.ui.totop
+//= require theme/jquery.flexslider-min
+//= require theme/jquery.scrollto
+//= require theme/owl.carousel
+//= require theme/jquery.countTo
+//= require theme/classie
+//= require theme/video.resize
+//= require theme/validation
+//= require theme/jquery.stellar.min
+//= require theme/designesia
+//= require theme/jquery.themepunch.plugins.min
+//= require theme/jquery.themepunch.revolution.min
+
+//= require_self
+
