79af7c80ca870b9f2a75f62e686cedf13a5c7c1c
Pothem.Mallesham <pothem.mallesham@gmail.com>
2018-06-14 23:37:31 +0530

    disabled puma  workers

--- config/puma.rb
+++ config/puma.rb
@@ -15,7 +15,7 @@
 #
 # environment ENV.fetch("RAILS_ENV") { "development" }
 
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+# workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
